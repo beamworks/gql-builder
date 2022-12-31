c5d673310b96f0c01eb82e52b7b20ac371504eeb
Nick Matantsev <nick.matantsev@gmail.com>
2022-12-30 19:24:41 -0500

    Clean up and rearrange types

--- src/query.ts
+++ src/query.ts
@@ -1,3 +1,10 @@
+// GraphQL types to JS types
+type FieldTypeMap = {
+  "String!": string;
+  "Number!": number;
+  "ID!": string;
+};
+
 // variable input definition object
 const VAR_MARKER = Symbol("var marker");
 type VarDefinition<VarName extends string, VarType extends string> = {
@@ -12,20 +19,6 @@ export function input<VarName extends `$${string}`, VarType extends string>(
     [VAR_MARKER]: [varName, varType],
   };
 }
-
-// using the weird "ask to TS keep strings narrow" trick from:
-// https://stackoverflow.com/questions/59440453/dynamically-generate-return-type-based-on-array-parameter-of-objects-in-typescri
-// and discussed here: https://github.com/microsoft/TypeScript/issues/30680
-type Definitions<MagicNarrowString extends string> = {
-  [key: string]:
-    | MagicNarrowString
-    | Definitions<MagicNarrowString>
-    | OpDefinition<
-        MagicNarrowString | null,
-        OpParamDefs,
-        Definitions<MagicNarrowString>
-      >;
-};
 
 // operation params map
 interface OpParamDefs {
@@ -73,6 +66,20 @@ export function op(
   return { [OP_MARKER]: [opName, params, defs] };
 }
 
+// using the weird "ask to TS keep strings narrow" trick from:
+// https://stackoverflow.com/questions/59440453/dynamically-generate-return-type-based-on-array-parameter-of-objects-in-typescri
+// and discussed here: https://github.com/microsoft/TypeScript/issues/30680
+type Definitions<MagicNarrowString extends string> = {
+  [key: string]:
+    | MagicNarrowString
+    | Definitions<MagicNarrowString>
+    | OpDefinition<
+        MagicNarrowString | null,
+        OpParamDefs,
+        Definitions<MagicNarrowString>
+      >;
+};
+
 export declare function query<
   Defs extends Definitions<MagicNarrowString>,
   MagicNarrowString extends string
@@ -80,19 +87,23 @@ export declare function query<
   defs: Defs // top level, like anything, can be simple fields, ops, etc
 ): Runner<Defs>;
 
-type VarsBareNames<Vars> = {
-  [T in keyof Vars as T extends `$${infer BareName}`
-    ? BareName
-    : never]: Vars[T];
-};
+// type VarsBareNames<Vars> = {
+//   [T in keyof Vars as T extends `$${infer BareName}`
+//     ? BareName
+//     : never]: Vars[T];
+// };
 
-type VarKeyValue<Def> = Def extends VarDefinition<infer VarName, infer VarType>
+// present a known variable as type { [varName]: varType }
+type VarAsKeyValue<Def> = Def extends VarDefinition<
+  infer VarName,
+  infer VarType
+>
   ? { [k in VarName]: FieldTypeMap[Extract<VarType, keyof FieldTypeMap>] }
   : never;
 
 // get a union of { param: type } variable info objects from definitions
 // (note: `Field extends string` ternary seems to be required, otherwise recursion fails
-type VarsFromDefs<
+type VarsForDefs<
   Defs extends Definitions<any>,
   Field = keyof Defs
 > = Field extends string
@@ -103,8 +114,8 @@ type VarsFromDefs<
         infer OpParams,
         infer OpFields
       >
-    ? VarKeyValue<OpParams[keyof OpParams]> | VarsFromDefs<OpFields>
-    : VarsFromDefs<Defs[Field]>
+    ? VarAsKeyValue<OpParams[keyof OpParams]> | VarsForDefs<OpFields>
+    : VarsForDefs<Defs[Field]>
   : never;
 
 // more evil magic: https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type
@@ -114,27 +125,22 @@ type UnionToIntersection<Union> = (
   ? Intersection
   : never;
 
+export interface Runner<Defs extends Definitions<string>> {
+  run(vars: UnionToIntersection<VarsForDefs<Defs>>): OutputForDefs<Defs>;
+}
+
+// utility to infer used variable names from defined query
 export type RunnerVars<R extends Runner<Definitions<any>>> = R extends Runner<
   infer Defs
 >
-  ? UnionToIntersection<VarsFromDefs<Defs>>
+  ? UnionToIntersection<VarsForDefs<Defs>>
   : never;
 
-interface Runner<Defs extends Definitions<string>> {
-  run(vars: UnionToIntersection<VarsFromDefs<Defs>>): RunnerOutput<Defs>;
-}
-
-type FieldTypeMap = {
-  "String!": string;
-  "Number!": number;
-  "ID!": string;
-};
-
 // interpret the collected query definitions
-type RunnerOutput<Defs> = {
+type OutputForDefs<Defs> = {
   [Field in keyof Defs]: Defs[Field] extends string
     ? FieldTypeMap[Extract<Defs[Field], keyof FieldTypeMap>]
     : Defs[Field] extends OpDefinition<infer OpName, any, infer OpFields>
-    ? RunnerOutput<OpFields>
-    : RunnerOutput<Defs[Field]>;
+    ? OutputForDefs<OpFields>
+    : OutputForDefs<Defs[Field]>;
 };
